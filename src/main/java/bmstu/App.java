3383ff96fbb02aa239b027c2aa5371a01cbbedad
GromkovAlexander <gromkovalex@mail.ru>
2019-12-26 19:03:03 +0300

    Scripted auto-commit on change (2019-12-26 19:03:03) by gitwatch.sh

--- src/main/java/bmstu/App.java
+++ src/main/java/bmstu/App.java
@@ -32,13 +32,16 @@ public class App {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
-        final ActorMaterializer materializer =
-                ActorMaterializer.create(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <
+
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
         );
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
-        
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
     }
 }
